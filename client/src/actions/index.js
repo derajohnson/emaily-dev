237f0fd3840b302010d21b3f6f2a0e9bac7b4724
derajohnson <53873657+derajohnson@users.noreply.github.com>
2020-03-08 23:46:14 +0100

    Create index.js

---
+++ client/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+import {FETCH_USER} from './types'
+
+export const fetchUser = () =>{
+    return function(dispatch){
+        return axios
+        .get('http://localhost:5000/api/current_user')
+        .then((res) => dispatch ({type: FETCH_USER, payload: res}))
+    }
+}
